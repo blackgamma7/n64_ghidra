57b11e4466bd29ff2074cad8301322db3001fbd7
blackgamma7 <28585957+blackgamma7@users.noreply.github.com>
2026-04-20 23:00:43 -0400

    Update NameConstFloats.java
    add more constant definitions, exclude unitialized data, remove invisible characters.

--- NameConstFloats.java
+++ NameConstFloats.java
@@ -31,33 +31,34 @@ public class NameConstFloats extends GhidraScript {
 		Data data = getFirstData();
 		//first iteration: find floats based on operand
 		while ((data != null) && (!monitor.isCancelled())) {
+		try{
 		String n=data.getDataType().getName();
 		if (n.equals("undefined4")||n.equals("undefined8")){
+		println(data.getMinAddress().toString());
 		Reference[] refs=symbolTable.getPrimarySymbol(data.getMinAddress()).getReferences();
 		if(refs.length>0){
 				for(Reference ref:refs){
 				println(ref.getFromAddress().toString());
-				try{
-				  String opc=listing.getCodeUnitAt​(ref.getFromAddress()).getMnemonicString().toLowerCase();
-				  if(opc.equals("lwc1")||opc.equals("_lwc1")||opc.equals("swc1")){
-				  listing.clearCodeUnits​(data.getMinAddress(),data.getMaxAddress(),false);
-					  listing.createData​(data.getMinAddress(),new FloatDataType());break;}
-				  if(opc.equals("ldc1")||opc.equals("_ldc1")||opc.equals("sdc1")){
-				  listing.clearCodeUnits​(data.getMinAddress(),data.getMaxAddress(),false);
-				listing.createData​(data.getMinAddress(),new DoubleDataType());break;}}
-				catch(Exception e){}
+				  String opc=listing.getCodeUnitAt(ref.getFromAddress()).getMnemonicString().toLowerCase();
+				  if(opc.equals("lwc1")||opc.equals("_lwc1")||opc.equals("swc1")||opc.equals("MOVSS")||opc.equals("FLD m32fp")){
+				  listing.clearCodeUnits(data.getMinAddress(),data.getMaxAddress(),false);
+					  listing.createData(data.getMinAddress(),new FloatDataType());break;}
+				  if(opc.equals("ldc1")||opc.equals("_ldc1")||opc.equals("sdc1")||opc.equals("MOVSD")||opc.equals("FLD m64fp")){
+				  listing.clearCodeUnits(data.getMinAddress(),data.getMaxAddress(),false);
+				listing.createData(data.getMinAddress(),new DoubleDataType());break;}
 		}}
-		}
+		}}
+		catch(Exception e){}
 		data = getDataAfter(data);
 		}
 		data = getFirstData();
 		//second: name them if they're constant.
 		try{
-			symbolTable.createNameSpace(symbolTable.getNamespace​("Global",null),"ConstFloats",SourceType.USER_DEFINED);
+			symbolTable.createNameSpace(symbolTable.getNamespace("Global",null),"ConstFloats",SourceType.USER_DEFINED);
 		}
 		catch(Exception e){}
 		while ((data != null) && (!monitor.isCancelled())) {
-			if (!data.isPointer() &&
+			if (!data.isPointer() && data.isInitializedMemory() &&
 				(data.getBaseDataType().getName().toLowerCase().equals("float")) ||
 				(data.getBaseDataType().getName().toLowerCase().equals("double"))) {
 				Symbol sym = symbolTable.getPrimarySymbol(data.getMinAddress());
@@ -77,6 +78,7 @@ public class NameConstFloats extends GhidraScript {
 					switch(val){
 					case "1.5707965":
 					case "1.5707964897155762":
+					case "1.5707963267948966":
 					newlabel="PIover2_"+ForD;
 					break;
 					case "0.016666668":
@@ -91,13 +93,26 @@ public class NameConstFloats extends GhidraScript {
 					case "3.1415927":
 					case "3.141593" :
 					case "3.14" :
+					case "3.141592653589793":
 					newlabel="PI_"+ForD;
 					break;
+					case "-3.141592653589793":
+					newlabel="NegPI_"+ForD;
+					break;
 					case "6.2831855" :
-					newlabel="2PI_"+ForD;
+					case "6.283185307179586":
+					newlabel="TAU_"+ForD;
 					break;
 					case "0.017453":
+					case "0.017453292519943295":
+					case "0.017453292":
 					newlabel="DtoR_"+ForD;
+					break;
+					case "0.7853981633974483":
+					newlabel="DtoR45_"+ForD;
+					break;
+					case "-0.7853981633974483":
+					newlabel="NegDtoR45_"+ForD;
 					break;
 					case "2.71828" :
 					newlabel="E_"+ForD;
@@ -116,6 +131,13 @@ public class NameConstFloats extends GhidraScript {
 					break;
 					case "65535.0":
 					newlabel="USHORT_MAX_"+ForD;
+					break;
+					case "1.4426950408889634":
+					newlabel="LOG2E_"+ForD;
+					break;
+					case "0.6931471805599453":
+					case "0.69314718246":
+					newlabel="LN2_"+ForD;
 					break;
 					default:
 					newlabel=val+ForD;
